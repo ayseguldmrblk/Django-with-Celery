a199c703ecc43fd4751beb80625f8093c0637336
Ayşe Gül Demirbilek <ayse.gul5212@gmail.com>
2023-09-11 12:18:31 +0300

    first commit

---
+++ feedback/tasks.py
@@ -0,0 +1,16 @@
+from time import sleep
+from django.core.mail import send_mail
+from celery import shared_task
+
+
+@shared_task()
+def send_feedback_email_task(email_address, message):
+
+    sleep(20)
+    send_mail(
+        "Your Feedback",
+        f"\t{message}\n\nThank You!",
+        "support@example.com",
+        [email_address],
+        fail_silently=False,
+    )
